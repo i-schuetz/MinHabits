687f112fa0ef08a2636257ffeaabd4d1ae86ab4a
Ivan Schuetz <ivan.schuetz@ottogroup.com>
2018-12-26 21:27:22 +0100

    Add nav bar to add habit modal

--- js/components/EditHabitView.js
+++ js/components/EditHabitView.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import NavigationBar from 'react-native-navbar';
 import Repo from '../Repo.js';
 
 export default class EditHabitView extends Component {
     render() {
+        const leftButtonConfig = {
+            title: 'x',
+            handler: () => this.props.onClose()
+        };
+        
+        const titleConfig = {
+            title: 'Add habit',
+        };
         return (
-            <View style={{marginTop: 22}}>
+            <View>
+                <NavigationBar
+                    title={titleConfig}
+                    leftButton={leftButtonConfig}
+                />
                 <View>
                 <Text>Hello World!</Text>
 
