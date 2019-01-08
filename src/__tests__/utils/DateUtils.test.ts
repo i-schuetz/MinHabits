4f39460c594340c3f5728bd201547fbdaf859df9
Ivan Schuetz <ivan.schuetz@ottogroup.com>
2019-01-08 19:30:45 +0100

    Test, fix and format DateUtils.
    Formatting should have been in a separate commit.

---
+++ src/__tests__/utils/DateUtils.test.ts
@@ -0,0 +1,149 @@
+import { DateUtils } from "../../utils/DateUtils"
+import { Weekday } from "../../models/Weekday"
+import { Month } from "../../models/Month"
+import { DayDate } from "../../models/DayDate"
+import { TimeUnit } from "../../models/TimeUnit"
+import TimeInterval from "../../models/TimeInterval"
+
+describe("DateUtils", () => {
+  it("Gets weekday from DayDate", () => {
+    expect(DateUtils.getWeekday({ day: 1, month: Month.January, year: 2019 })).toEqual(Weekday.Tuesday)
+    expect(DateUtils.getWeekday({ day: 2, month: Month.January, year: 2019 })).toEqual(Weekday.Wednesday)
+    expect(DateUtils.getWeekday({ day: 3, month: Month.January, year: 2019 })).toEqual(Weekday.Thursday)
+    expect(DateUtils.getWeekday({ day: 4, month: Month.January, year: 2019 })).toEqual(Weekday.Friday)
+    expect(DateUtils.getWeekday({ day: 5, month: Month.January, year: 2019 })).toEqual(Weekday.Saturday)
+    expect(DateUtils.getWeekday({ day: 6, month: Month.January, year: 2019 })).toEqual(Weekday.Sunday)
+    expect(DateUtils.getWeekday({ day: 7, month: Month.January, year: 2019 })).toEqual(Weekday.Monday)
+    expect(DateUtils.getWeekday({ day: 29, month: Month.February, year: 2016 })).toEqual(Weekday.Monday) // TODO confirm
+  })
+
+  it("Gets end from DayDate", () => {
+    expect(DateUtils.getEnd({ day: 1, month: Month.January, year: 2019 }, { value: 1, unit: TimeUnit.Day })).toEqual({
+      day: 2,
+      month: Month.January,
+      year: 2019
+    } as DayDate)
+
+    expect(DateUtils.getEnd({ day: 1, month: Month.January, year: 2019 }, { value: 10, unit: TimeUnit.Day })).toEqual({
+      day: 11,
+      month: Month.January,
+      year: 2019
+    } as DayDate)
+
+    expect(DateUtils.getEnd({ day: 1, month: Month.January, year: 2019 }, { value: 40, unit: TimeUnit.Day })).toEqual({
+      day: 10,
+      month: Month.February,
+      year: 2019
+    } as DayDate)
+
+    expect(DateUtils.getEnd({ day: 1, month: Month.January, year: 2019 }, { value: 1, unit: TimeUnit.Week })).toEqual({
+      day: 8,
+      month: Month.January,
+      year: 2019
+    } as DayDate)
+
+    expect(DateUtils.getEnd({ day: 1, month: Month.January, year: 2019 }, { value: 8, unit: TimeUnit.Week })).toEqual({
+      day: 26,
+      month: Month.February,
+      year: 2019
+    } as DayDate) // TODO confirm
+
+    expect(
+      DateUtils.getEnd({ day: 15, month: Month.February, year: 2019 }, { value: 10, unit: TimeUnit.Month })
+    ).toEqual({
+      day: 15,
+      month: Month.December,
+      year: 2019
+    } as DayDate)
+
+    expect(
+      DateUtils.getEnd({ day: 15, month: Month.February, year: 2019 }, { value: 12, unit: TimeUnit.Month })
+    ).toEqual({
+      day: 15,
+      month: Month.February,
+      year: 2020
+    } as DayDate)
+
+    expect(DateUtils.getEnd({ day: 18, month: Month.June, year: 2019 }, { value: 100, unit: TimeUnit.Year })).toEqual({
+      day: 18,
+      month: Month.June,
+      year: 2119
+    } as DayDate)
+  })
+
+  it("DayDate is in interval", () => {
+    expect(
+      DateUtils.isInInterval(
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 1, month: Month.January, year: 2019 },
+        { value: 2, unit: TimeUnit.Day }
+      )
+    ).toEqual(true)
+
+    expect(
+      DateUtils.isInInterval(
+        { day: 1, month: Month.February, year: 2019 },
+        { day: 2, month: Month.January, year: 2019 },
+        { value: 1, unit: TimeUnit.Month }
+      )
+    ).toEqual(true)
+  })
+
+  it("DayDate is not in interval", () => {
+    // Exclusive interval - day is not in previous day + 1 day interval!
+    expect(
+      DateUtils.isInInterval(
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 1, month: Month.January, year: 2019 },
+        { value: 1, unit: TimeUnit.Day }
+      )
+    ).toEqual(false)
+
+    // Exclusive interval - day is not in previous month + 1 month interval!
+    expect(
+      DateUtils.isInInterval(
+        { day: 1, month: Month.February, year: 2019 },
+        { day: 1, month: Month.January, year: 2019 },
+        { value: 1, unit: TimeUnit.Month }
+      )
+    ).toEqual(false)
+  })
+
+  it("DayDate is in start end", () => {
+    expect(
+      DateUtils.isInStartEnd(
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 1, month: Month.January, year: 2019 },
+        { day: 3, month: Month.January, year: 2019 }
+      )
+    ).toEqual(true)
+
+    expect(
+      DateUtils.isInStartEnd(
+        { day: 1, month: Month.February, year: 2019 },
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 2, month: Month.February, year: 2019 }
+      )
+    ).toEqual(true)
+  })
+
+  it("DayDate is not in end", () => {
+    // Exclusive interval - day is not in previous day + 1 day interval!
+    expect(
+      DateUtils.isInStartEnd(
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 1, month: Month.January, year: 2019 },
+        { day: 2, month: Month.January, year: 2019 }
+      )
+    ).toEqual(false)
+
+    // Exclusive interval - day is not in previous month + 1 month interval!
+    expect(
+      DateUtils.isInStartEnd(
+        { day: 1, month: Month.February, year: 2019 },
+        { day: 2, month: Month.January, year: 2019 },
+        { day: 1, month: Month.February, year: 2019 }
+      )
+    ).toEqual(false)
+  })
+})
--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,48 +1,48 @@
-import * as moment from 'moment-timezone';
-import { DayDate } from '../models/DayDate';
-import { Month } from '../models/Month';
-import { TimeUnit } from '../models/TimeUnit';
-import TimeInterval from '../models/TimeInterval';
-import { Weekday } from '../models/Weekday';
+import * as moment from "moment-timezone"
+import { DayDate } from "../models/DayDate"
+import { Month } from "../models/Month"
+import { TimeUnit } from "../models/TimeUnit"
+import TimeInterval from "../models/TimeInterval"
+import { Weekday } from "../models/Weekday"
 
 export namespace DateUtils {
-  
-  const dayDateFormat = "DD-MM-YYYY" 
+  const dayDateFormat = "DD-MM-YYYY"
   const timezone = "UTC" // Use always UTC to prevent timezone convertion.
 
   export function getWeekday(date: DayDate): Weekday {
-    return toWeekday(toMomentFromDayDate(date).isoWeekday());
+    return toWeekday(toMomentFromDayDate(date).isoWeekday())
   }
-  
+
   export function getEnd(start: DayDate, interval: TimeInterval): DayDate {
-    const endDate = toMomentFromDayDate(start).add(1, toMomentUnitString(interval.unit))
-    return { day: endDate.day(), month: toMonth(endDate.month()), year: endDate.year() }
-  } 
-  
-  export function isInInterval(date: DayDate, start: DayDate, timeInterval: TimeInterval): boolean {
-    const end = getEnd(start, timeInterval);
-    return isInStartEnd(date, start, end);
+    const endDate = toMomentFromDayDate(start).add(interval.value, toMomentUnitString(interval.unit))
+    return { day: endDate.date(), month: toMonth(endDate.month()), year: endDate.year() }
   }
-  
+
+  export function isInInterval(date: DayDate, start: DayDate, timeInterval: TimeInterval): boolean {
+    const end = getEnd(start, timeInterval)
+    return isInStartEnd(date, start, end)
+  }
+
   export function isInStartEnd(date: DayDate, start: DayDate, end: DayDate): boolean {
-    return toMomentFromDayDate(date).isBetween(toMomentFromDayDate(start), toMomentFromDayDate(end));
+    return toMomentFromDayDate(date).isBetween(toMomentFromDayDate(start), toMomentFromDayDate(end))
   }
 
   /**
    * @param dayDate date to convert
    * @returns typle with string representation and format string.
    */
-  export function toDayDateString(dayDate: DayDate): {formatted: string, format: string} {
+  export function toDayDateString(dayDate: DayDate): { formatted: string; format: string } {
     return {
-      formatted: toMomentFromDayDate(dayDate).format(dayDateFormat), 
+      formatted: toMomentFromDayDate(dayDate).format(dayDateFormat),
       format: dayDateFormat
-    };
+    }
   }
 
   export function parseDayDate(str: string): DayDate {
-    const mom = toMomentFromString(str, dayDateFormat);
-    
-    if (JSON.stringify(mom) == "null") { // Validation hack - if str is invalid, mom will be NaN, but the compiler doesn't let us check for NaN!
+    const mom = toMomentFromString(str, dayDateFormat)
+
+    if (JSON.stringify(mom) == "null") {
+      // Validation hack - if str is invalid, mom will be NaN, but the compiler doesn't let us check for NaN!
       throw new Error(`Couldn't parse day date string: ${str}`)
     }
 
@@ -57,54 +57,111 @@ export namespace DateUtils {
    * Converts moment lib month index to Month
    */
   function parseMonth(index: number): Month {
-    return Month.parseNumber(index);
+    return Month.parseNumber(index)
   }
 
   function toWeekday(momentIsoWeekday: number): Weekday {
     switch (momentIsoWeekday) {
-      case 1: return Weekday.Monday;
-      case 2: return Weekday.Tuesday;
-      case 3: return Weekday.Wednesday;
-      case 4: return Weekday.Thursday;
-      case 5: return Weekday.Friday;
-      case 6: return Weekday.Saturday;
-      case 7: return Weekday.Sunday;
-      default: throw new Error(`Invalid iso weekday: ${momentIsoWeekday}`)
+      case 1:
+        return Weekday.Monday
+      case 2:
+        return Weekday.Tuesday
+      case 3:
+        return Weekday.Wednesday
+      case 4:
+        return Weekday.Thursday
+      case 5:
+        return Weekday.Friday
+      case 6:
+        return Weekday.Saturday
+      case 7:
+        return Weekday.Sunday
+      default:
+        throw new Error(`Invalid iso weekday: ${momentIsoWeekday}`)
     }
   }
 
   function toMomentUnitString(unit: TimeUnit): moment.DurationInputArg2 {
     switch (unit) {
-      case TimeUnit.Day: return "days";
-      case TimeUnit.Week: return "weeks";
-      case TimeUnit.Month: return "months";
-      case TimeUnit.Year: return "years";
+      case TimeUnit.Day:
+        return "days"
+      case TimeUnit.Week:
+        return "weeks"
+      case TimeUnit.Month:
+        return "months"
+      case TimeUnit.Year:
+        return "years"
     }
   }
 
-  // TODO index is wrong - test!
   function toMonth(momentMonth: number) {
     switch (momentMonth) {
-      case 1: return Month.January;
-      case 2: return Month.February;
-      case 3: return Month.March;
-      case 4: return Month.April;
-      case 5: return Month.May;
-      case 6: return Month.June;
-      case 7: return Month.July;
-      case 8: return Month.August;
-      case 9: return Month.September;
-      case 10: return Month.October;
-      case 11: return Month.November;
-      case 12: return Month.December;
-      default: throw new Error(`Invalid moment month: ${momentMonth}`) 
+      case 0:
+        return Month.January
+      case 1:
+        return Month.February
+      case 2:
+        return Month.March
+      case 3:
+        return Month.April
+      case 4:
+        return Month.May
+      case 5:
+        return Month.June
+      case 6:
+        return Month.July
+      case 7:
+        return Month.August
+      case 8:
+        return Month.September
+      case 9:
+        return Month.October
+      case 10:
+        return Month.November
+      case 11:
+        return Month.December
+      default:
+        throw new Error(`Invalid moment month: ${momentMonth}`)
+    }
+  }
+
+  function toMomentMonth(month: Month) {
+    switch (month) {
+      case Month.January:
+        return 0
+      case Month.February:
+        return 1
+      case Month.March:
+        return 2
+      case Month.April:
+        return 3
+      case Month.May:
+        return 4
+      case Month.June:
+        return 5
+      case Month.July:
+        return 6
+      case Month.August:
+        return 7
+      case Month.September:
+        return 8
+      case Month.October:
+        return 9
+      case Month.November:
+        return 10
+      case Month.December:
+        return 11
     }
   }
 
   function toMomentFromDayDate(dayDate: DayDate): moment.Moment {
     // Note: Setters have to be called in decresing order (year-month-date) for leap years to work properly.
     // See https://github.com/moment/moment/issues/3041
-    return moment.tz(timezone).year(dayDate.year).month(dayDate.month).date(dayDate.day);
+    return moment
+      .tz(timezone)
+      .year(dayDate.year)
+      .month(toMomentMonth(dayDate.month))
+      .date(dayDate.day)
   }
 
   function toMomentFromString(formatted: string, format: string): moment.Moment {
