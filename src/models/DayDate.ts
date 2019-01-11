b11c47132596fc08c32b429041cb4188e6f079f4
Ivan Schuetz <ivan.schuetz@ottogroup.com>
2019-01-11 11:43:27 +0100

    More compact implementation of compareTo

--- src/models/DayDate.ts
+++ src/models/DayDate.ts
@@ -19,37 +19,18 @@ export namespace DayDate {
   }
 
   export function compare(dayDate1: DayDate, dayDate2: DayDate): Order {
-    // function toOrder(difference: number): Order {
-    //   return difference == 0 ? Order.EQ : (difference < 0 ? Order.LT : Order.GT)
-    // }
-    // switch (toOrder(dayDate1.year - dayDate2.year)) {
-    //   case Order.LT: return Order.LT
-    //   case Order.GT: return Order.GT
-    //   case Order.EQ: {
-    //     switch (toOrder(dayDate1.month - dayDate2.month)) {
-    //     case Order.LT: return Order.LT
-    //     case Order.GT: return Order.GT
-    //     case Order.EQ: return (toOrder(dayDate1.day - dayDate2.day)) 
-    //   }
-    // }
-    if (dayDate1.year < dayDate2.year) {
-      return Order.LT 
-    } else if (dayDate1.year > dayDate2.year) {
-      return Order.GT
-    } else { // Same year
-      if (dayDate1.month < dayDate2.month) {
-        return Order.LT
-      } else if (dayDate1.month > dayDate2.month) {
-        return Order.GT
-      } else { // Same month
-        if (dayDate1.day < dayDate2.day) {
-          return Order.LT
-        } else if (dayDate1.day > dayDate2.day) {
-          return Order.GT
-        } else {
-          return Order.EQ
-        }
-      }
+
+    function numCompare(x: number, y: number) {
+      if(x > y) return Order.GT;
+      if(x < y) return Order.LT;
+      return Order.EQ;
     }
+  
+    const result = [
+      numCompare(dayDate1.year, dayDate2.year),
+      numCompare(dayDate1.month, dayDate2.month),
+      numCompare(dayDate1.day, dayDate2.day),
+    ].find(o => o !== Order.EQ) 
+    return result === undefined ? Order.EQ : result
   }
 }
