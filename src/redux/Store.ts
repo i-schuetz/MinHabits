2fbe3a0aa33f17b9478e186269504d6180bc843e
Ivan Schuetz <ivan.schuetz@ottogroup.com>
2019-01-26 20:14:31 +0100

    Install redux devtools

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,11 +1,16 @@
-import { Store, createStore, applyMiddleware } from 'redux'
-import rootReducer, { ApplicationState } from './reducers/RootReducer';
-import thunk from 'redux-thunk';
+import { Store, createStore, applyMiddleware, compose } from "redux"
+
+import rootReducer, { ApplicationState } from "./reducers/RootReducer"
+import thunk from "redux-thunk"
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default function configureStore(): Store<ApplicationState> {
-  const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-  )
+  const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
   return store
 }
